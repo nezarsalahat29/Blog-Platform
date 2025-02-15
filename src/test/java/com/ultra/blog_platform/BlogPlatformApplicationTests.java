ad929190179952ece856f435d9ca48f22309a0de
Nezar Al-Salahat <nezarsalahat29@gmail.com>
2025-02-15 11:49:28 +0300

    Initial commit

---
+++ src/test/java/com/ultra/blog_platform/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultra.blog_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
